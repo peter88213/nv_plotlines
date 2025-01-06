086e57d1c51831e09ffbd17527445e8dff961c40
peter88213 <petert88213@gmail.com>
2025-01-06 23:06:43 +0100

    Update docstrings.

--- src/nvplotlines/nvplotlines_locale.py
+++ src/nvplotlines/nvplotlines_locale.py
@@ -1,6 +1,6 @@
 """Localization for nv_plotlines.
 
-Copyright (c) 2024 Peter Triesberger
+Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/nv_plotlines
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
