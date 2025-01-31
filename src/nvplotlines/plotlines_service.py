b0514ee1ec46baa1d7547aee1230888e3adce9a2
peter88213 <petert88213@gmail.com>
2025-01-31 22:21:35 +0100

    Refactor
    - Abandoning mvclib

--- src/nvplotlines/plotlines_service.py
+++ src/nvplotlines/plotlines_service.py
@@ -6,7 +6,7 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from pathlib import Path
 
-from mvclib.controller.sub_controller import SubController
+from nvlib.controller.sub_controller import SubController
 
 
 class PlotlinesService(SubController):
